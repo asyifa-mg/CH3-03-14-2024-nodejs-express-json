93a0f690d026de126b3f2601405cbc7b2604d9e9
asyifa-mg <asyifamaharanigustina12@gmail.com>
2026-03-04 14:24:49 +0700

    change

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // inisialisasi untuk membaca data json
 const customers = JSON.parse(fs.readFileSync(`${__dirname}/data/dummy.json`));
 
+// ================== GET ================================
 //anonymous function menggunakan parameter req,res,next
 app.get("/", (req, res, next) => {
   res.send("<h1>Hello FSW 1 Tercinta..</h1>");
@@ -17,6 +18,7 @@ app.get("/", (req, res, next) => {
 
 // memanggil data dummy json yg ada di folder data
 // /api/v1/customers  => merupakan penamaan yg sesuai dengan aturan API dimana nama api terus versi dan collection atau datanya
+//api get all data
 app.get("/api/v1/customers", (req, res, next) => {
   res.status(200).json({
     status: "success",
@@ -27,15 +29,84 @@ app.get("/api/v1/customers", (req, res, next) => {
   });
 });
 
+//api utk get data by id menggunakan params untuk memanggil parameter
+app.get("/api/v1/customers/:id", (req, res, next) => {
+  console.log(req.params);
+  console.log(req.params.id);
+  const id = req.params.id;
+
+  //menggunakan array method untuk mendapatkan data spesifik
+  const customer = customers.find((cust) => cust._id === id);
+  console.log(customer);
+
+  //shortcut pemanggilan objek dengan params lebih simpel
+  // const { id } = req.params;
+  // console.log(id);
+
+  //===================
+  res.status(200).json({
+    status: "success",
+    data: {
+      customer,
+    },
+  });
+});
+
+// ================== PATCH ================================
+// API untuk update data
+//put mengharuskan update semua objek data
+//patch hanya spesifik data yg mau diupdate
+app.patch("/api/v1/customers/:id", (req, res) => {
+  console.log("masuk ga?");
+  const id = req.params.id;
+
+  //1. lakukan pencarian data yang sesuai parameter id nya
+  const customer = customers.find((cust) => cust._id === id);
+  const customerIndex = customers.findIndex((cust) => cust._id === id);
+
+  console.log(customer);
+  console.log(customerIndex);
+  console.log(!customer);
+
+  // 2. ada gak data customer nya?
+  if (!customer) {
+    //pencarian jika data tdk ada
+    return res.status(404).json({
+      status: "fail",
+      message: `Customer dengan ID: ${id}" gak ada`,
+    });
+  }
+
+  //3. kalau ada berarti update data nya sesuai request body dari client/user
+  // object assign = menggabungkan object or spread operator (...)
+  customers[customerIndex] = { ...customers[customerIndex], ...req.body };
+  console.log(customers[customerIndex]);
+
+  //4. melakukan update didokumen json nya
+  fs.writeFile(
+    `${__dirname}/data/dummy.json`,
+    JSON.stringify(customers),
+    (err) => {
+      res.status(200).json({
+        status: "success",
+        message: "Berhasil update data",
+      });
+    },
+  );
+});
+
+// ================== POST ================================
 // app.post("/api/v1/customers", (req, res) => {
 //   console.log(req.body);
 //   res.send("selesai"); //harus ada akhiran res dalam rest api dengan express agar tidak mutar terus req nya atau load
 // });
 
-app.post("/api/v1/customers", (req, res) => {
+//api post atau insert data dummy
+app.post("/api/v1/customers/", (req, res) => {
   console.log(req.body);
 
   const newCustomer = req.body;
+
   customers.push(newCustomer);
 
   fs.writeFile(
@@ -56,7 +127,7 @@ app.listen(PORT, () => {
   console.log(`APP running on port : ${PORT}`);
 });
 
-// //api utk get data by id
+// //api utk get data by id menggunakan params untuk memanggil parameter
 // const getCustomersById = (req, res, next) => {
 //   //shorhcut pemanggilan objek
 //   const id = req.params.id;
